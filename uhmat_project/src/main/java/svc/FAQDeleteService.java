bcb866a12938962672a71defd2ddccb4106802c8
navy160523 <106544538+navy160523@users.noreply.github.com>
2022-08-07 01:23:38 +0900

    서비스센터(FAQ, Notice) 초안 (#14)
    * 2022.08.05 02:55
    
    * 2022.08.06 10:07
    
    * 2022.08.06 10:13
    
    * 댓글 제외하고 ServiceCenterFrontcontroller 완성
    
    * 댓글 넣는거까지는 성공했음... 출력을 못하고 있음
    
    * 카테고리 드롭박스 만들기 말고는 다 구현함
    
    * 카테고리 드롭박스 만드는 것 말고는 다함.
    
    * 서비스센터 게시판(faq, notice) 초안
    
    * 서비스센터(FAQ, Notice) 초안(모든 기능 오류 없음)

---
+++ uhmat_project/src/main/java/svc/FAQDeleteService.java
@@ -0,0 +1,32 @@
+package svc;
+
+import java.sql.*;
+
+import dao.*;
+import db.*;
+
+public class FAQDeleteService {
+
+	public boolean removeFAQ(int idx) {
+		System.out.println("FAQDeleteService - removeFAQ");
+		boolean deleteSuccess = false;
+		
+		Connection con = JdbcUtil.getConnection();
+		
+		FAQDAO dao = FAQDAO.getInstance();
+		
+		dao.setConnection(con);
+		
+		deleteSuccess = dao.deleteFAQ(idx);
+		System.out.println("deleteSuccess : "+ deleteSuccess);
+		
+		if(!deleteSuccess) {
+			JdbcUtil.rollback(con);
+		} else {
+			JdbcUtil.commit(con);
+		}
+		
+		return deleteSuccess;
+	}
+
+}
