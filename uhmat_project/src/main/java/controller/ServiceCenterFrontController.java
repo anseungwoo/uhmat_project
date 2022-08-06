f071f031d0f23c45fcb46d17fadeade33fc8ad13
youngjae <navy160523@gmail.com>
2022-08-06 10:13:50 +0900

    2022.08.06 10:13

--- uhmat_project/src/main/java/controller/ServiceCenterFrontController.java
+++ uhmat_project/src/main/java/controller/ServiceCenterFrontController.java
@@ -132,8 +132,8 @@ public class ServiceCenterFrontController extends HttpServlet {
 				}
 		}
 		
-		
 		//--------------------------------------------------------------------
+		
 		if (forward != null) {
 			if (forward.isRedirect()) {
 				response.sendRedirect(forward.getPath());
